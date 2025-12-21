3bb7916605b4048e54a6f38efd1908af3dbc95cb
ephremM <mandefroephrem48@gmail.com>
2025-12-22 02:47:19 +0300

    Add tripdotloader

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -1,16 +1,13 @@
 <template>
   <Loader v-if="isLoading" />
-  <StepGradientLoader  class="text-black"/>
+  <CircularLoader  class="text-black"/>
 
   <NuxtPage />
 
 </template>
 
 <script setup>
-import Loader from "../../src/runtime/components/Loader.vue";
-import { useLoader } from "../../src/runtime/composables/useLoader";
 import CircularLoader from "../../src/templates/CircularLoader.vue"
-import StepGradientLoader from "../../src/templates/StepGradientLoader.vue"
 
 const config = useLoader();
 const { isLoading } = config;
