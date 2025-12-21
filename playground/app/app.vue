695dc716cdef0818f9b07a9a1b6b17364c3aa9ab
ephremM <mandefroephrem48@gmail.com>
2025-12-22 02:25:51 +0300

    Add: PulseRailLoader component with unique dot-bar animation

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <Loader v-if="isLoading" />
-  <CircularLoader  class="text-black"/>
+  <DotsLoader  class="text-black"/>
 
   <NuxtPage />
 
@@ -10,6 +10,7 @@
 import Loader from "../../src/runtime/components/Loader.vue";
 import { useLoader } from "../../src/runtime/composables/useLoader";
 import CircularLoader from "../../src/templates/CircularLoader.vue"
+import DotsLoader from "../../src/templates/DotsLoader.vue"
 
 const config = useLoader();
 const { isLoading } = config;
